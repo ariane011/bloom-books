b79d9251ee80f91a31fc1e5bd04224e9db90b401
Ariane Sousa <ariane.sousa011@gmail.com>
2023-04-27 00:40:54 -0300

    Criando useContext

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+const UserContext = React.createContext();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserContext.Provider>
+      <App />
+    </UserContext.Provider>
   </React.StrictMode>
 );
